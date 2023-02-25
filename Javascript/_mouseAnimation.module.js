f2968d4d21e22155abcf76eba4b213085c36f4be
iMuhammadMustafa <iMuhammadMustafa@gmail.com>
2023-02-25 20:16:26 +0200

    Create _mouseAnimation.module.js

---
+++ Javascript/_mouseAnimation.module.js
@@ -0,0 +1,10 @@
+document.querySelector(".skill-grid").onmousemove = e => {
+  for (const container of document.querySelectorAll(".skill-container")) {
+    const rect = container.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    container.style.setProperty("--mouse-x", x + "px");
+    container.style.setProperty("--mouse-y", y + "px");
+  }
+};
