08f734a44f4fc7907017d79053bda7871bd15251
iMuhammadMustafa <iMuhammadMustafa@gmail.com>
2023-03-03 03:32:39 +0200

    Update Hero Section
    - Add Dev Icons
    - Fix Flex Layout
    - Add svh unit
    - Add intro padding
    - Update index.html

--- Javascript/_mouseAnimation.module.js
+++ Javascript/_mouseAnimation.module.js
@@ -8,3 +8,14 @@ document.querySelector(".skill-grid").onmousemove = e => {
     container.style.setProperty("--mouse-y", y + "px");
   }
 };
+
+document.querySelector(".hero-grid").onmousemove = e => {
+  for (const container of document.querySelectorAll(".card-hoverable")) {
+    const rect = container.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    container.style.setProperty("--mouse-x", x + "px");
+    container.style.setProperty("--mouse-y", y + "px");
+  }
+};
