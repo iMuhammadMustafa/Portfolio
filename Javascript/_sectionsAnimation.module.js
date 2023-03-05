12e8f20222c3e572f51a70d535fd6892f5852909
iMuhammadMustafa <iMuhammadMustafa@gmail.com>
2023-03-05 02:27:53 +0200

    Add Experience Section

--- Javascript/_sectionsAnimation.module.js
+++ Javascript/_sectionsAnimation.module.js
@@ -1,10 +1,10 @@
 const hiddenSections = document.querySelectorAll(".hidden-section");
-const sectionsObserver = new IntersectionObserver(
+const hiddenParts = document.querySelectorAll(".hidden-part");
+const sectionObserver = new IntersectionObserver(
   (entries, observer) => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
         entry.target.classList.remove("hidden-section");
-        //observer.unobserve(entry.target);
       } else {
         entry.target.classList.add("hidden-section");
       }
@@ -12,6 +12,24 @@ const sectionsObserver = new IntersectionObserver(
   },
   { threshold: [0.3] }
 );
+partsObserver = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.remove("hidden-part");
+      } else {
+        entry.target.classList.add("hidden-part");
+      }
+    });
+  },
+  { threshold: [0.3] }
+);
+
 hiddenSections.forEach(section => {
-  sectionsObserver.observe(section);
+  sectionObserver.observe(section);
 });
+hiddenParts.forEach(part => {
+  partsObserver.observe(part);
+});
+
+//observer.unobserve(entry.target);
