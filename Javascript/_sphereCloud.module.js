c736b66c7da7f7d1df805121683d5ee7aa82d262
iMuhammadMustafa <iMuhammadMustafa@gmail.com>
2023-04-04 11:17:31 +0200

    Update _sphereCloud.module.js

--- Javascript/_sphereCloud.module.js
+++ Javascript/_sphereCloud.module.js
@@ -3,7 +3,7 @@ const cloudCanvas = document.querySelector(".cloud-sphere");
 // const texts = ["Javascript", "HTML5", ".NET", "C#", "TypeScript", "CSS", "PostgreSQL", "MongoDB", "React", "NextJS", "NodeJS", "Redux"];
 const texts = [...document.querySelectorAll(".cloud-list>li")].map(el => el.textContent);
 
-options = {
+const options = {
   maxSpeed: "normal",
   initSpeed: "normal",
   radius: 300,
