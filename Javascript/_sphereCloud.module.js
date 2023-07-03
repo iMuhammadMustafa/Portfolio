e57739453dccb097fa582f9ca593e8ae84102ef0
iMuhammadMustafa <29302054+iMuhammadMustafa@users.noreply.github.com>
2023-07-03 05:08:29 +0300

    Fix Media Queires and remove comments
    - Add Fav Icon.
    - Fix Testmonials on phones.
    - Fix Word Cloud on phones.

--- Javascript/_sphereCloud.module.js
+++ Javascript/_sphereCloud.module.js
@@ -1,12 +1,12 @@
 const cloudCanvas = document.querySelector(".cloud-sphere");
 
 // const texts = ["Javascript", "HTML5", ".NET", "C#", "TypeScript", "CSS", "PostgreSQL", "MongoDB", "React", "NextJS", "NodeJS", "Redux"];
-const texts = [...document.querySelectorAll(".cloud-list>li")].map(el => el.textContent);
+const texts = [...document.querySelectorAll(".cloud-list>li")].map((el) => el.textContent);
 
 const options = {
   maxSpeed: "normal",
   initSpeed: "normal",
-  radius: 300,
+  radius: 250,
 };
 
 TagCloud(cloudCanvas, texts, options);
