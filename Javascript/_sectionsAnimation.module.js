6a2db73bd69654b3f78a8183e71b42c29181b3f9
iMuhammadMustafa <iMuhammadMustafa@gmail.com>
2024-04-24 19:41:01 -0500

    Update Animations and Added Under Construction

--- Javascript/_sectionsAnimation.module.js
+++ Javascript/_sectionsAnimation.module.js
@@ -1,37 +1,50 @@
 const hiddenSections = document.querySelectorAll(".hidden-section");
 const hiddenParts = document.querySelectorAll(".hidden-part");
-const sectionObserver = new IntersectionObserver(
-  (entries, observer) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.remove("hidden-section");
-      }
-      // else {
-      //   entry.target.classList.add("hidden-section");
-      // }
-    });
+
+window.addEventListener(
+  "load",
+  function load(e) {
+    window.removeEventListener("load", load, false);
+    this.setTimeout(() => {
+      run_observe();
+    }, 100);
   },
-  { threshold: [0.3] }
-);
-const partsObserver = new IntersectionObserver(
-  (entries, observer) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.remove("hidden-part");
-      }
-      // else {
-      //   entry.target.classList.add("hidden-part");
-      // }
-    });
-  },
-  { threshold: [0.3] }
+  false
 );
 
-hiddenSections.forEach(section => {
-  sectionObserver.observe(section);
-});
-hiddenParts.forEach(part => {
-  partsObserver.observe(part);
-});
+async function run_observe() {
+  const sectionObserver = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.remove("hidden-section");
+        }
+        // else {
+        //   entry.target.classList.add("hidden-section");
+        // }
+      });
+    },
+    { threshold: [0.3] }
+  );
+  const partsObserver = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.remove("hidden-part");
+        }
+        // else {
+        //   entry.target.classList.add("hidden-part");
+        // }
+      });
+    },
+    { threshold: [0.3] }
+  );
 
+  hiddenSections.forEach(section => {
+    sectionObserver.observe(section);
+  });
+  hiddenParts.forEach(part => {
+    partsObserver.observe(part);
+  });
+}
 //observer.unobserve(entry.target);
